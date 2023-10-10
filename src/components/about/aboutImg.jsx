52e16d8adc86a293f1b85b97b2840204c7a7e5eb
Raven <Rivaldo.dodo2306@gmail.com>
2023-10-10 21:38:23 +0700

    fix shadow

--- src/components/about/aboutImg.jsx
+++ src/components/about/aboutImg.jsx
@@ -3,11 +3,16 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 import Image1 from '../../images/img-1.jpg';
 const AboutImage = () => {
     return (
-        <>
-         <div className="about-founder md:col-span-1 md:w-3/4 mx-auto">
-          <LazyLoadImage src={Image1} alt="" className='w-full rounded-xl shadow-xl shadow-slate-400' effect='blur'/>
+      <>
+        <div className="about-founder md:col-span-1 md:w-3/4 mx-auto">
+          <LazyLoadImage
+            src={Image1}
+            alt=""
+            className="w-full rounded-xl shadow-xl shadow-black-100"
+            effect="blur"
+          />
         </div>
-        </>
-    )
+      </>
+    );
 }
 export default AboutImage
